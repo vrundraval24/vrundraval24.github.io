a6cab6e5e44e2e519d0f8ba334a7d3da6e4385d0
Vrund <vrundraval24@gmail.com>
2023-09-11 22:45:43 +0530

    updated

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -40,8 +40,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "3c4b79ce8aca4fdc961398da54c9c7d7
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "0673cca043b00d2d56aa90da25dc68be",
-"/": "0673cca043b00d2d56aa90da25dc68be",
+"index.html": "476f9ddf7f3268d50b2a66f064aeab9c",
+"/": "476f9ddf7f3268d50b2a66f064aeab9c",
 "main.dart.js": "7a92f4432a8bdd86e0988a4821779907",
 "manifest.json": "e74af8957b5899dc6da961caee768ec9",
 "version.json": "009c9e65172e010890f7f65fde438006"};
